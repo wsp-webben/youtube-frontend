e69c1d7f7ff59ca00496509443755b62dfde04ec
webben <wspwebben@gmail.com>
2019-03-05 16:52:08 +0700

    fixed dobuled components

---
+++ src/components/Index.vue
@@ -0,0 +1,12 @@
+<template>
+  <section class="Index">
+    <h1 class="Index__title">If you like YouTube, but hate red</h1>
+    <slot></slot>
+  </section>
+</template>
+
+<style lang="scss">
+  .Index__title {
+    color: var(--main-color);
+  }
+</style>
---
+++ src/components/VideoListItem.vue
@@ -0,0 +1,7 @@
+<template>
+  <li class="VideoListItem">
+    <span class="VideoListItem__title">
+      <slot></slot>
+    </span>
+  </li>
+</template>
