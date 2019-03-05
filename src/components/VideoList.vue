e1a0e4051f7aca43704372f51d1f1673822ec002
webben <wspwebben@gmail.com>
2019-03-05 16:51:32 +0700

    removed dobled files

--- src/components/VideoList.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <section class="VideoList">
-    <ul class="VideoList__list">
-      <slot></slot>
-    </ul>
-  </section>
-</template>
--- src/components/VideoListItem.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <li class="VideoListItem">
-    <span class="VideoListItem__title">
-      <slot></slot>
-    </span>
-  </li>
-</template>
