142a9ee81f91f4520102cc2a0e0f280a6cb04610
webben <wspwebben@gmail.com>
2019-03-12 10:59:15 +0700

    rewritten server

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const port = process.env.PORT || 8080
-const app = express()
+const serveStatic = require('serve-static');
+const path = require('path');
 
-app.use(express.static(__dirname + '/dist/'));
-app.get('/.*/', function(req, res) {
-  res.sendfile(__dirname + '/dist/index.html');
-});
-app.listen(port)
+const app = express();
+app.use('/', serveStatic(path.join(__dirname, '/dist')));
 
-console.log('Server started: ' + port);
+app.get('*', function (req, res) {
+  res.sendFile(__dirname + '/dist/index.html')
+})
+
+const port = process.env.PORT || 8080;
+app.listen(port);
+
+console.log(`Server started on port ${port}`);
